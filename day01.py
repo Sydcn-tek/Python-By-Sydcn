289348d683db552744243da822d1180bf7f94f44
Sydcn-tek <51552425+Sydcn-tek@users.noreply.github.com>
2019-09-10 08:33:13 +0800

    Add files via upload

---
+++ day01.py
@@ -0,0 +1,105 @@
+# email='775446425@qq.com'
+# for a in email:
+#     x=ord(a)-10
+#     print(chr(x),end="")
+
+
+
+# year = int(input('请输入年份：>>'))
+# if(year % 4 == 0 and year % 100 != 0) or( year % 400 == 0) :
+#     print('%d 是闰年'%year)
+# else:
+#     print('%d 不是闰年'%year)
+
+
+
+
+# f=float(input('请输入华氏温度：'))
+# c=(f-32)/1.8
+# print('%.2f华氏度 = %.2f摄氏度' % (f, c))
+
+
+
+# number =input('请输入数字：')
+# bai = int(number[0])
+# shi = int(number[1])
+# ge = int(number[2])
+# if bai **3+shi **3+ge**3 ==int (number):
+#     print ('是水仙花')
+# else:
+#     print('不是水仙花')
+
+# 1.
+# c=float(input('请输入摄氏温度：'))
+# f=C*1.8+32
+# print('%.2f华氏度'%f)
+
+# 2.
+# import math
+# r=float(input('请输入圆的半径：'))
+# h=float(input('请输入圆柱体的高：'))
+# area=math.pi*r*r
+# V=math.pi*r*r*h
+# print('面积：%.4f'%area)
+# print('体积：%.4f'%V)
+
+
+# 3.
+# feet=float(input('请输入英尺数:'))
+# m=feet*0.305
+# print('%.4f米'%m)
+
+
+
+
+# 4.
+# m=float(input('请输入水的千克数：'))
+# t1=float(input('请输入最终温度：'))
+# t2=float(input('请输入初始温度：'))
+# Q=m*(t1-t2)*4184
+# print('%.1f焦耳'%Q)
+
+
+# 5.
+# c=float(input('请输入差额：'))
+# n=float(input('请输入年利率'))
+# l=c*(n/1200)
+# print('%.5f元'%l)
+
+
+
+# 6.
+# v0=float(input('请输入初速度：'))
+# v1=float(input('请输入末速度：'))
+# t=float(input('请输入所占用时间'))
+# a=(v1-v0)/t
+# print('%.4f'%a)
+
+
+
+# 7.
+# n=float(input('请输入每月存款数：'))
+# m=1
+# a=n*1.00417
+# print(a)
+# while m<=5:
+#     a=(a+100)*1.00417
+#     m+=1
+# print('六个月后的账户总额：', a)
+
+# 8.
+# number=input('请输入一个0~1000的数字：')
+# b=int(number[0])
+# s=int(number[1])
+# g=int(number[2])
+# a=b+s+g
+# print('各个数字之和：%d'%a)
+
+
+
+
+
+
+
+
+
